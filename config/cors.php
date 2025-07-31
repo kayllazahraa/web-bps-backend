989ffb884a8b097762ec1b87ce2b69a4c10a605a
kayllazahraa <168892439+kayllazahraa@users.noreply.github.com>
2025-08-01 02:21:45 +0700

    edit cors lagi

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'https://bps-bengkulu-kaylla-pi.vercel.app'],
+    'allowed_origins' => ['http://localhost:5173', 'https://bps-bengkulu.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
